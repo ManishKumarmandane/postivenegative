54c36a58ee889dc5cbc892444d8b252b2a818360
manishkumarmandane <bm0369@gmail.com>
2022-07-26 21:19:26 +0100

    postive

---
+++ PostiveNegative.java
@@ -0,0 +1,17 @@
+package IFelsecond;
+
+public class PostiveNegative
+{
+    public static void main(String[] args)
+    {
+        int num = -10;
+        if(num>0)
+        {
+            System.out.println("postive");
+        }
+        else
+        {
+            System.out.println("negative");
+        }
+    }
+}

833a99276a6e494a857782307e19a86d7ffbf178
manishkumarmandane <bm0369@gmail.com>
2022-07-26 21:26:48 +0100

    modi

--- PostiveNegative.java
+++ PostiveNegative.java
@@ -4,15 +4,16 @@ public class PostiveNegative
 {
     public static void main(String[] args)
     {
-        int num = 10;
+        int num = -10;
         if(num>0)
         {
             System.out.println("postive");
-            System.out.println("hari");
+         //   System.out.println("hari");
         }
         else
         {
             System.out.println("negative");
+            System.out.println("566");
         }
     }
 }
